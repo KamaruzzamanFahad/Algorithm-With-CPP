bf2f3427e7d840d2f997978ab7b6af0c77e94ed6
Kamaruzzaman Fahad <58844727+KamaruzzamanFahad@users.noreply.github.com>
2025-02-20 08:47:13 +0600

    disktara optimize

--- Algorithom_Week_02_Cycle_Detection_&_Dijkstra/dijkstara_optimize.cpp
+++ Algorithom_Week_02_Cycle_Detection_&_Dijkstra/dijkstara_optimize.cpp
@@ -2,13 +2,27 @@
 using namespace std;
 int n,e;
 vector<pair<int,int>> adj_list[100];
+int dis[100];
 
-int main(){
+
+void disktara(int src){
+    priority_queue<pair<int,int>, vector<pair<int,int>> , greater<pair<int,int>>> pq;
     cin >> n,e;
+}
 
+int main() {
     while(e--){
-        
-    }
+        int a,b,c;
+        cin >> a >> b >> c;
+        adj_list[a].push_back({b,c});
+        adj_list[b].push_back({a,c});
+        }
+
+    fill(dis, dis+100, 500);
+
+    disktara(0);
+
+
   
   return 0;
 }
